f1273237183f869018a4ad18b11a0d0cddee966b
Roy Veshovda <roy@veshovda.no>
2016-10-20 05:13:23 +0200

    Update config.exs

--- apps/logic/config/config.exs
+++ apps/logic/config/config.exs
@@ -21,6 +21,13 @@ use Mix.Config
 #     config :logger, level: :info
 #
 
+config :extwitter, :oauth, [
+   consumer_key: "aaa",
+   consumer_secret: "bbb",
+   access_token: "ccc",
+   access_token_secret: "ddd"
+]
+
 # It is also possible to import configuration files, relative to this
 # directory. For example, you can emulate configuration per environment
 # by uncommenting the line below and defining dev.exs, test.exs and such.
