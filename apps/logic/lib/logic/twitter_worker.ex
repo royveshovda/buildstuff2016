5c4ef559bce33dc9714a5e19c881aeab9aa709b4
Roy Veshovda <roy@veshovda.no>
2016-10-22 05:27:13 +0200

    Improve startup of twitter_worker

--- apps/logic/lib/logic/twitter_worker.ex
+++ apps/logic/lib/logic/twitter_worker.ex
@@ -15,17 +15,35 @@ defmodule Logic.TwitterWorker do
   end
 
   def init([]) do
-    worker = self()
+    Process.send_after(self(), :starting, 500)
+    {:ok, %{}}
+  end
+
+  def handle_info(:starting, state) do
+    case DateTime.utc_now.year do
+      1970 ->
+        Process.send_after(self(), :starting, 500)
+        {:noreply, state}
+      _ ->
+        worker = self()
+        pid = start_twitter_stream(worker)
+        {:noreply, %{pid: pid}}
+    end
+  end
+
+  defp start_twitter_stream(pid_to_send_result) do
     pid = spawn(fn ->
       Process.sleep(15000)
       IO.puts "Listening to Twitter"
       stream = ExTwitter.stream_filter(track: @hashtag)
       for tweet <- stream do
-        GenServer.cast(worker, {:tweet_update, tweet})
+        GenServer.cast(pid_to_send_result, {:tweet_update, tweet})
       end
     end)
-    {:ok, {%{pid: pid}}}
+    pid
   end
+
+
 
   def handle_cast({:tweet_update, tweet}, state) do
     message = String.replace(tweet.text, @hashtag, "")
