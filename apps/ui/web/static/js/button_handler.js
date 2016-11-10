9148aa9d396e6d68913eebf615e5bcb2b19ffea0
saklig <frank@frankmac-3.local>
2016-11-10 19:23:46 +0100

    buttons should now reflect hardware state

--- apps/ui/web/static/js/button_handler.js
+++ apps/ui/web/static/js/button_handler.js
@@ -7,36 +7,42 @@ export function HandleButtonSignal(payload) {
   let temperatureButton = document.getElementById('btn_temparature');
   let humidityButton = document.getElementById('btn_humidity');
 
+  if (payload.body.button_updated === 'b1') {
+    if (payload.body.temperature_button === 'up') {
+      // setTimeout(function () {
+      //     temperatureButton.className = 'btn btn-default'
+      // }, 1000);
 
-  if (payload.body.temperature_button === 'up') {
-    setTimeout(function () {
-        temperatureButton.className = 'btn btn-default'
-    }, 1000);
+      temperatureButton.className = 'btn btn-default'
+    }
 
-    temperatureButton.className = 'btn btn-success'
+    if (payload.body.temperature_button === 'down') {
+      // setTimeout(function () {
+      //     temperatureButton.className = 'btn btn-default'
+      // }, 1000);
+
+      temperatureButton.className = 'btn btn-danger'
+    }
   }
 
-  if (payload.body.temperature_button === 'down') {
-    setTimeout(function () {
-        temperatureButton.className = 'btn btn-default'
-    }, 1000);
+  if (payload.body.button_updated === 'b2') {
+    if (payload.body.humidity_button === 'up') {
+      // setTimeout(function () {
+      //     humidityButton.className = 'btn btn-default'
+      // }, 1000);
 
-    temperatureButton.className = 'btn btn-danger'
+      humidityButton.className = 'btn btn-default'
+    }
+
+    if (payload.body.humidity_button === 'down') {
+      // setTimeout(function () {
+      //     humidityButton.className = 'btn btn-default'
+      // }, 1000);
+
+      humidityButton.className = 'btn btn-danger'
+    }
   }
 
-  if (payload.body.humidity_button === 'up') {
-    setTimeout(function () {
-        humidityButton.className = 'btn btn-default'
-    }, 1000);
 
-    humidityButton.className = 'btn btn-success'
-  }
 
-  if (payload.body.humidity_button === 'down') {
-    setTimeout(function () {
-        humidityButton.className = 'btn btn-default'
-    }, 1000);
-
-    humidityButton.className = 'btn btn-danger'
-  }
 }
